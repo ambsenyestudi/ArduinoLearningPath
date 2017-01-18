a5ed7e38c74bba571896e4a4682e179c6c03434a
Ambseny <ambsenyestudi@outlook.com>
2017-01-18 01:32:50 -0800

    State Implementation

--- ArduinoLearningPath.ino
+++ ArduinoLearningPath.ino
@@ -1,17 +1,46 @@
-int btn1 = 5;
+//type definition
+#define BUTTON 5
+typedef enum
+{
+	READY, PUSHED, RELEASED
+}BUTTON_STATE;
+//fields
+BUTTON_STATE myButtonState = READY;
 void setup()
 {
 	//Use default led
 	pinMode(LED_BUILTIN, OUTPUT);
 	//Use dthis this pin for button
-	pinMode(btn1, INPUT_PULLUP);
+	pinMode(BUTTON, INPUT_PULLUP);
 	//SetUp Seriall port at 9600 bitrate
 	Serial.begin(9600);
 }
 
 void loop()
 {
-	int btn1val = digitalRead(btn1);
+	int btn1val = digitalRead(BUTTON);
+	//button state change
+	switch (myButtonState)
+	{
+		case READY:
+			if (btn1val == HIGH)
+			{
+				myButtonState = PUSHED;
+			}
+			break;
+		case PUSHED:
+			if (btn1val == LOW)
+			{
+				myButtonState = RELEASED;
+			}
+			break;
+		case RELEASED:
+			myButtonState = READY;
+			Serial.println("Button Released ");
+			break;
+		default:
+		break;
+	}
 	//button pushed 
 	if (btn1val == 1)
 	{
@@ -21,6 +50,5 @@ void loop()
 	{
 		digitalWrite(LED_BUILTIN, LOW);
 	}
-	Serial.print("Button state ");
-	Serial.println(btn1val);
+	
 }
