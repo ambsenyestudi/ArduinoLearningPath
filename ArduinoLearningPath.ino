7da69ba2752b63ff150b0aefc318fea8dbdfdec0
Ambseny <ambsenyestudi@outlook.com>
2017-01-18 00:54:00 -0800

    Add project files.

---
+++ ArduinoLearningPath.ino
@@ -0,0 +1,30 @@
+
+int interval = 300;
+int timer = 0;
+
+void setup()
+{
+	//Use default led
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+	//millis() returns ellapsed time in millisecons
+	int currTime = millis() - timer;
+	if (currTime > interval)
+	{
+		//digitalRead Gets if the pin is on or of
+		if (digitalRead(LED_BUILTIN) == LOW)
+		{
+			digitalWrite(LED_BUILTIN, HIGH);
+		}
+		else
+		{
+			digitalWrite(LED_BUILTIN, LOW);
+		}
+		//Now timer is updated at this very time
+		timer = millis();
+	}
+
+}
