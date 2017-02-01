e49a038efcdd5d7e0be3c70f501bad2c157b6b5d
Ambseny <ambsenyestudi@outlook.com>
2017-02-01 17:21:31 +0100

    Lits twice as much time than it stays unlit

--- ArduinoLearningPath.ino
+++ ArduinoLearningPath.ino
@@ -1,11 +1,13 @@
 
-int interval = 300;
+const int INITIALINTERVAL = 500;
+int interval = 0;
 int timer = 0;
 //This is an example of how to avoid using delay()
 void setup()
 {
 	//Use default led
 	pinMode(LED_BUILTIN, OUTPUT);
+	interval = INITIALINTERVAL;
 }
 
 void loop()
@@ -18,10 +20,13 @@ void loop()
 		if (digitalRead(LED_BUILTIN) == LOW)
 		{
 			digitalWrite(LED_BUILTIN, HIGH);
+			interval = INITIALINTERVAL;
 		}
 		else
 		{
+			//spends half of lit time
 			digitalWrite(LED_BUILTIN, LOW);
+			interval = INITIALINTERVAL/2;
 		}
 		//Now timer is updated at this very time
 		timer = millis();
